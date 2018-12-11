3ee94f022540476afb40512e9a99c431c42e625a
Willis Allstead <willallstead@icloud.com>
2018-12-10 20:59:15 -0800

    Added My games page with db and create game page is functional

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,39 +1,11 @@
 $(document).ready(function() {
-	validateEmail(); 
-    $("[data-toggle=popover]").popover(); // Enables popover
-});
-
-
-
-
-function validateEmail(){
-	$('.input-group input[required]').on('keyup change', function() {
-    	var $form = $(this).closest('form'),
-		$group = $(this).closest('.input-group'),
-		$addon = $group.find('.input-group-addon'),
-		$icon = $addon.find('span'),
-		state = false;
-		
-    	if (!$group.data('validate')) {
-			state = $(this).val() ? true : false;
-			}else if ($group.data('validate') == "email") {
-			state = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test($(this).val())
-		}
-		
-		if (state) {
-			$addon.removeClass('danger');
-			$addon.addClass('success');
-			}else{
-			$addon.removeClass('success');
-			$addon.addClass('danger');
-		}
-        
-        if ($form.find('.input-group-addon.danger').length == 0) {
-            $form.find('[type="submit"]').prop('disabled', false);
-			}else{
-            $form.find('[type="submit"]').prop('disabled', true);
-		}
+	
+	$('#add-word').on('click', function () {
+		var wordsDiv = $('.wordsInGame');
+		wordsDiv.append('<div class="uk-margin"><div class="uk-inline"><input class="uk-input" type="text" name="words" /><button class="uk-form-icon uk-form-icon-flip delete-word" uk-icon="icon: close" type="button"></button></div></div>');
 	});
-    
-    $('.input-group input[required], .input-group textarea[required], .input-group select[required]').trigger('change');
-}
+
+	$('.spelling-game ').on('click','.delete-word', function() {
+		$(this).closest( ".uk-margin" ).remove();
+	});
+});
