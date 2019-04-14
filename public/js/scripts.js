13ca4f39fea56bc9ae9a07f154a2c4320ac8369a
Willis Allstead <willallstead@icloud.com>
2019-04-14 12:06:52 -0700

    2-way communication between game and website is working better, cleaned up a bit

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -13,63 +13,22 @@ $(document).ready(function () {
 		$(this).closest(".uk-margin").remove();
 	});
 
-	// var unityObjectUrl = "http://webplayer.unity3d.com/download_webplayer-3.x/3.0/uo/UnityObject2.js";
-	// if (document.location.protocol == 'https:')
-	// 	unityObjectUrl = unityObjectUrl.replace("http://", "https://ssl-");
-	// document.write('<script type="text/javascript" src="http://webplayer.unity3d.com/download_webplayer-3.x/3.0/uo/UnityObject2.js"></script>');
-
-	// function SayHello(arg) {
-	// 	// show the message
-	// 	alert(arg);
-	// }
-
-	// var u = new UnityObject2();
-	// console.log($(".new-game-frame")[0])
-	var myGame = UnityLoader.instantiate($(".new-game-frame")[0], "http://localhost:5000/public/games/spelling/Build/testBuild.json", {
-		onProgress: myProgress
-	});
-
+	
 	function myProgress(gameInstance, progress) {
-		// console.log(progress)
 		if (progress == 1) {
 			console.log("game is loaded")
 			console.log(gameInstance)
 			gameInstance.SendMessage("Main Camera", "SetGameData", "Hello from the web page!");
-			myGame.SendMessage("Main Camera", "SetGameData", "Hello from the web page2!");
-			// SendMessage("Main Camera", "SetGameData", "Hello from the web page3!");
 		}
 	}
 
-	console.log(myGame)
+	function loadGame() {
+		var myGame = UnityLoader.instantiate($(".new-game-frame")[0], "http://localhost:5000/public/games/spelling/Build/testBuild.json", {
+			onProgress: myProgress
+		});
+	}
 
-	// myGame.onProgress(function (progress) {
-	// 	console.log("progress: " + progress)
-	// 	var $missingScreen = jQuery(progress.targetEl).find(".missing");
-	// 	switch (progress.pluginStatus) {
-	// 		case "unsupported":
-	// 			// u.showUnsupported();
-	// 			break;
-	// 		case "broken":
-	// 			alert("You will need to restart your browser after installation.");
-	// 			break;
-	// 		case "missing":
-	// 			$missingScreen.find("a").click(function (e) {
-	// 				e.stopPropagation();
-	// 				e.preventDefault();
-	// 				u.installPlugin();
-	// 				return false;
-	// 			});
-	// 			$missingScreen.show();
-	// 			break;
-	// 		case "installed":
-	// 			$missingScreen.remove();
-	// 			break;
-	// 		case "first":
-	// 			console.log("started")
-	// 			myGame.SendMessage("TextText", "SetGameData", "Hello from the web page!");
-	// 			break;
-	// 	}
-	// });
-
-	// u.initPlugin(jQuery(".gameframe")[0], "Example.unity3d");
+	if ($(".new-game-frame").length) {
+		loadGame()
+	}
 });
